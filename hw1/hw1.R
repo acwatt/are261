4447078b3c163a5e6b55c55fa956d4cad69e81e7
Aaron Watt <aaron@acwatt.net>
2021-09-07 12:27:25 -0700

    adding hw1 files

---
+++ hw1/hw1.R
@@ -0,0 +1,23 @@
+## ---------------------------
+## R version 3.6.3 (2020-02-29)
+## Script name: hw1.R
+##
+## Purpose of script: run code necessary for hw1
+##
+## Author: Aaron Watt
+## Email: aaron@acwatt.net
+##
+## Date Created: 2021-09-07
+##
+## ---------------------------
+## Notes:
+##
+
+
+## PACKAGES ===========================================
+# install.packages("pacman")
+# install.packages("car", repos="http://R-Forge.R-project.org")
+library(fs)
+
+## WORKING DIRECTORIES ===============================
+hw_path = path('/run/user/1000/gvfs/google-drive:host=gmail.com,user=kratzerer.canby/0Ber.cer.canby/0B5hVoKD1E_AtMmMwMDMyY2UtOTAyYS00Njc4LWI1NGMtNDc4ZTFmMDMyZmY5/1FJoBCb0FBD5bYXIRFvLnkDzv0vLnkDzv0S9BWOBD5bYXIRFFg/1jZhisi9n0nKx8psDNUlF61klUCXml96X/1tKdocO7U8iP99zMGn2f1oKK6MxyIl7vdS-RaXxyIl7vdS-RaX/1lp_FNCWCHlPQwAoKK6MX2ac82tSvpMQY/1JiwxSGFs_WsI-VSGS1dSYCFazHETphU8')
